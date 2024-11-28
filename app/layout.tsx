86397a378df53e8ad8d6a8f5ca6893cb78237b82
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-11-28 09:07:48 +0700

    first init

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Candra Wali S | Portfolio',
+    template: '%s - Candra Wali S | Portfolio',
+  },
+  description: 'Portfolio Candra Wali Sanjaya, seorang Front-End Developer profesional.',
+  category: 'Portfolio, Front-End Developer',
+  authors: [{ name: 'chndrwali' }],
+  keywords: ['Portfolio', 'Front-End Developer', 'Candra Wali', 'Web Development'],
+  creator: 'Candra Wali Sanjaya',
+  publisher: 'Candra Wali Sanjaya',
+  twitter: {
+    card: 'summary_large_image',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
